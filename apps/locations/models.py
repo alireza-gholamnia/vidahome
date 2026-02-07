d6f65feb150c2639f0bf41c74d490dcdd6343291
alireza <gholamniareza@gmail.com>
2026-02-07 08:06:57 -0800

    refactor(search): rename resolver to city_context and add category/area landings

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from apps.categories.models import Category
+from django.core.exceptions import ValidationError
 
 class BaseLocation(models.Model):
     """
